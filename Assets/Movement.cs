050eadc85fbd7d4d64db980d063f3f3130777087
Oscar Fällgren Glemme <omg_ogge@hotmail.com>
2020-02-24 07:06:06 +1100

    worked on movement script

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,26 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Movement : MonoBehaviour
 {
 
-
-
-
+    public OVRInput.Controller goController;
+    public NavMeshAgent navAgent;
+    public Transform navDest;
+    public GameObject navTo;
 
 
     void Start()
     {
 
+        goController = OVRInput.Controller.RTrackedRemote;
+        navAgent = gameObject.GetComponent<NavMeshAgent>();
 
-        
+        if(navTo.activeSelf == false)
+        {
+
+            navTo.SetActive(true);
+
+        }
+
     }
 
     void Update()
     {
-      
-        
+        /*if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+
+            RaycastHit Hit;
+
+            //transform.rotation = OVRInput.GetLocalControllerRotation(goController);
+
+            if (Physics.Raycast(transform.position, transform.forward, out Hit, 50))
+            {
+                navDest = Hit.transform;
+                navAgent.destination = Hit.transform.position;
+
+            }
+
+        }*/
+
+        RaycastHit Hit;
+
+        //transform.rotation = OVRInput.GetLocalControllerRotation(goController);
+
+        if (Physics.Raycast(transform.position, transform.forward, out Hit, 50))
+        {
+
+            navTo.transform.position = Hit.transform.position;
+
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+            {
+
+                navDest = Hit.transform;
+                navAgent.destination = Hit.transform.position;
+
+            }                
+
+        }
 
     }
+
+    private void OnDrawGizmosSelected()
+    {
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(transform.position, transform.forward);
+
+    }
+
 }
