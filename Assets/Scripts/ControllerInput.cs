bc350a3578ad76a6358777e9881c5694c1f39a01
Phlip <1016942@student.sae.edu>
2020-03-03 16:52:47 +1100

    Magic Mechanics (Raycast/Projectile), InteractableObject Setups, Triggers, Gaze Setup

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour
+{
+    //Mouse&Keyboard inputs for Testing
+    //public AudioClip clip;
+    //public AudioSource audioSource;
+
+    public Transform controllerForwardTransform;
+    public GameObject magic;
+
+    public bool canShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // audioSource = GetComponent<AudioSource>()
+        // audioSource.clip = clip;
+    }
+    // Update is called once per frame
+
+    void Update()
+    {
+        //transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            if (canShoot == true)
+            {
+                MagicShoot();
+                Debug.Log("shoot");
+            }
+        }
+
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+
+        }
+
+        //Mouse&Keyboard inputs for Testing
+    }
+
+
+    private void MagicShoot()
+    {
+       //Raycast Shot
+        RaycastHit hit;
+
+        if (Physics.Raycast(controllerForwardTransform.position, controllerForwardTransform.forward, out hit))
+        {
+            if (hit.collider.gameObject.CompareTag("Interactable"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+
+        //Projectile Shot
+        GameObject.Instantiate(magic, controllerForwardTransform.position, controllerForwardTransform.forward);
+       // GetCompnent<Rigidbody>().AddForce.forward * 10;
+        
+
+
+    }
+}
---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+
+
+    public bool isMushroom;
+    public bool isTree;
+    public bool isWater;
+    public bool isFlower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Magic")
+        {
+            if (isMushroom == true)
+            {
+              //  gameObject.GetComponent<Transform>().Scale * 2;
+            }
+
+            else if (isTree == true)
+            {
+              //  gameObject.GetComponent<Transform>().Scale / 2;
+            }
+
+            else if (isWater == true)
+            {
+               // gameObject.GetComponent<Transform>().Scale * 2;
+
+            }
+
+            else if (isFlower == true)
+            {
+               // Destroy(gameObject);
+            }
+
+        }
+
+
+
+    }
+}
