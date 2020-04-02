d0b7edf71755172a6d506f907321226b76d81e60
Luca <lucatannler@hotmail.com>
2020-04-02 11:32:29 +1100

    Added a simple Rabbit/AI that hops around. (Super basic, unfinished, hacky)

---
+++ Assets/Scripts/AI/MovementController.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+namespace AI
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class MovementController : MonoBehaviour
+    {
+        private CharacterController _charController;
+
+        public float gravity = -9.81f;
+        public float jumpForce = 20;
+
+        public bool running = false;
+        
+        public Vector2 maxWalkSpeed = new Vector3(1,2); // Backward, Fwd
+        public Vector2 maxRunSpeed = new Vector3(2,3); // Backward, Fwd
+        public float turnSpeed = 45; //(Angles/Sec)
+        public float acceleration = 2;
+        public Vector3 inputMovement; // 0-1
+
+        private bool _isGrounded;
+        private float _distToGround = 0.5f;
+        
+        public AudioSource audioSource;
+        //public AudioClip movingSound;
+
+        private void Awake()
+        {
+            _charController = GetComponent<CharacterController>();
+
+            if(_charController != null)
+                _distToGround = _charController.bounds.extents.y;
+            //audioSource = GetComponent<AudioSource>();
+            //audioSource.clip = movingSound;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _isGrounded = IsGrounded();
+            Move();
+
+            if (audioSource == null)
+                return;
+            
+            // HACKY
+            if (_charController.velocity.magnitude > 0.1)
+            {
+                if(!audioSource.isPlaying)
+                    audioSource.Play();
+            }
+            else if(audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+            //Debug.Log(_charController.velocity);
+        }
+
+        private Vector3 _movementVector = new Vector3(0,0, 0);
+        protected virtual void Move()
+        {
+        
+            // TODO Handle Rotation
+            Vector3 rotationVector = Vector3.zero;
+            if (inputMovement.x > 0)
+                rotationVector.y = turnSpeed * Time.deltaTime;
+            else if(inputMovement.x < 0)
+                rotationVector.y = -turnSpeed * Time.deltaTime;
+            
+            transform.Rotate(rotationVector);
+            
+            // Movement
+
+            
+            if (inputMovement.z > 0)
+                _movementVector.z += acceleration * Time.deltaTime;
+            else if(inputMovement.z < 0)
+                _movementVector.z -= acceleration * Time.deltaTime;
+            else
+                _movementVector.z = Mathf.MoveTowards(_movementVector.x, 0, 2 * acceleration);
+            _movementVector.z = running ? Mathf.Clamp(_movementVector.z, -Mathf.Abs(maxRunSpeed.x), Mathf.Abs(maxRunSpeed.y)) : Mathf.Clamp(_movementVector.z, -Mathf.Abs(maxWalkSpeed.x), Mathf.Abs(maxWalkSpeed.y));
+
+            
+            Vector3 move = transform.TransformVector(_movementVector);
+            
+            if (inputMovement.y > 0 && _isGrounded)
+                move.y += jumpForce;
+            move.y = gravity;
+
+            _charController.SimpleMove(move);
+        }
+
+        private bool IsGrounded()
+        {
+            return Physics.Raycast(transform.position, Vector3.down, out var hit, _distToGround + 0.05f); // TODO
+        }
+    }
+}
---
+++ Assets/Scripts/AI/SimpleRabbitAI.cs
@@ -0,0 +1,256 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace AI
+{
+    /// <summary>
+    /// SUPER HACKY CHEAP RABBIT AI
+    /// </summary>
+    public class SimpleRabbitAI : MonoBehaviour
+    {
+        public bool debugEnableMovementInput;
+        
+        public bool scaredOfPlayer = false;
+        public float viewDistance = 10f;
+        public LayerMask obstacleLayers;
+        public List<string> scaredOfTags;
+
+        public float targetReachedThreshold = 2f;
+
+        public AudioSource audioSource;
+        //public AudioClip eatingSound;
+        public float eatingSoundDuration = 0;
+        public float eatingSoundCooldown = 0;
+        
+        private MovementController _movementController;
+
+        public enum AIState
+        {
+            Idle, HopAround, RunAway
+        }
+
+        public AIState defaultState = AIState.Idle;
+        public AIState currentState;
+        
+
+        private Vector3 _currentTargetLocation;
+        public float angleToTargetThreshold = 5;
+        public float angleToTargetMoveThreshold = 30; // Rabbit can only move & turn at the same time if angle to target is <= this angle
+        public float maxRotationStep = 30;
+
+
+        public float eatingTime = 4;
+        public float movingTime = 15;
+        private float eatingCountdown = 0;
+        private float movingCountdown = 0;
+
+        public float newTargetSearchRadius = 5;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _movementController = GetComponent<MovementController>();
+            currentState = defaultState;
+            //audioSource = GetComponent<AudioSource>();
+/*
+
+            if (eatingSound != null)
+                eatingSoundDuration = eatingSound.length;*/
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (eatingSoundCooldown > 0)
+                eatingSoundCooldown -= Time.deltaTime;
+            
+            if (debugEnableMovementInput)
+                DebugInputMovement();
+            
+            GetSensoryInput();
+            DoStateAction();
+
+        }
+
+        private void DebugInputMovement()
+        {
+            Vector3 inputMovement = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W))
+                inputMovement.z = 1;
+            else if (Input.GetKey(KeyCode.S))
+                inputMovement.z = -1;
+            
+            if (Input.GetKey(KeyCode.A))
+                inputMovement.x = -1;
+            else if (Input.GetKey(KeyCode.D))
+                inputMovement.x = 1;
+            
+            _movementController.inputMovement = inputMovement;
+        }
+
+        private void GetSensoryInput()
+        {
+            // Sensory Input
+            if (Physics.Raycast(transform.position, transform.forward, out var hit, viewDistance, obstacleLayers))
+            {
+                if (scaredOfTags?.Contains(hit.collider.tag) ?? false)
+                {
+                    
+                }
+            }
+        }
+
+        private void DoStateAction()
+        {
+            switch (currentState)
+            {
+                case AIState.HopAround: DoHopAroundState();
+                    break;
+                case AIState.RunAway: DoRunAwayState();
+                    break;
+                case AIState.Idle: break;
+            }
+        }
+
+        private void DoHopAroundState()
+        {
+
+            if (eatingCountdown > 0)
+            {
+                
+                eatingCountdown -= Time.deltaTime;
+
+                if (eatingCountdown <= 0)
+                {
+                    audioSource?.Stop();
+                    Debug.Log("STOP NOM");
+                    movingCountdown = movingTime;
+                    return;
+                }
+                
+                // Eating Code
+                _movementController.inputMovement.z = 0;
+                
+                // TODO Hacky, Inperformant
+                if (audioSource != null && !audioSource.isPlaying/* && eatingSoundCooldown <= 0*/)
+                {
+                    audioSource.Play();
+                    Debug.Log("PLAY NOM");
+                    /*audioSource.PlayOneShot(eatingSound, 1f);
+                    eatingSoundCooldown = eatingSoundDuration;*/
+                }
+                
+            }else if (movingCountdown > 0)
+            {
+                
+                movingCountdown -= Time.deltaTime;
+
+                if (movingCountdown <= 0)
+                {
+                    eatingCountdown = eatingTime;
+                    return;
+                }
+                
+                // Moving Code
+                var distToTarget = Vector3.Distance(transform.position, _currentTargetLocation);
+
+                if (distToTarget <= targetReachedThreshold)
+                {
+                    _currentTargetLocation = GetNewTargetLocation();
+                }
+                
+                MoveToTarget();
+            }
+            else
+            {
+                movingCountdown = movingTime;
+            }
+            
+        }
+
+        // Hacky
+        private Vector3 GetNewTargetLocation()
+        {
+            Vector3 newTargetLoc = Vector3.zero;
+
+
+
+            var sampleCounter = 0;
+            Vector3 newDir = Quaternion.AngleAxis(Random.Range(0, 360), transform.up) * transform.forward;
+            
+            while (Physics.Raycast(transform.position, newDir, out var hit, newTargetSearchRadius) && sampleCounter < 20)
+            {
+                newDir = Quaternion.AngleAxis(Random.Range(0, 360), transform.up) * transform.forward;
+                newTargetLoc = hit.point;
+                sampleCounter++;
+            }
+
+            if (newTargetLoc.Equals(Vector3.zero))
+            {
+                newTargetLoc = transform.position + newDir*newTargetSearchRadius;
+            }
+            
+            return newTargetLoc;
+        }
+
+        private void DoRunAwayState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /*private void RotateToTarget()
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,
+                Quaternion.LookRotation(_currentTargetLocation - transform.position), maxRotationStep);
+        }*/
+
+        public float angleToTarget;
+        private void MoveToTarget()
+        {
+            var groundedForwardDir = transform.forward;
+            groundedForwardDir.y = 0;
+            var groundedTargetDir = _currentTargetLocation - transform.position;
+            groundedTargetDir.y = 0;
+            angleToTarget = Vector3.SignedAngle(groundedForwardDir, groundedTargetDir,Vector3.up);
+            if (angleToTarget > angleToTargetThreshold)
+            {
+                _movementController.inputMovement.x = 1;
+                if(Mathf.Abs(angleToTarget) > angleToTargetMoveThreshold)
+                    _movementController.inputMovement.z = 0;
+                else
+                    _movementController.inputMovement.z = 1;
+            }
+            else if (angleToTarget < -angleToTargetThreshold)
+            {
+                _movementController.inputMovement.x = -1;
+                if(Mathf.Abs(angleToTarget) > angleToTargetMoveThreshold)
+                    _movementController.inputMovement.z = 0;
+                else
+                    _movementController.inputMovement.z = 1;
+            }
+            else
+            {
+                _movementController.inputMovement.x = 0;
+                _movementController.inputMovement.z = 1;
+            }
+            
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(_currentTargetLocation, 2);
+            
+            var distToTarget = Vector3.Distance(transform.position, _currentTargetLocation);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward*distToTarget);
+            
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(transform.position, Quaternion.AngleAxis(angleToTarget, transform.up) * transform.forward);
+        }
+    }
+}
